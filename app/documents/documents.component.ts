b305d675b4560429fad9255a3abc91af3c020c64
Jared Dunn <rellikdragon@yahoo.com>
2018-03-27 09:25:53 -0600

    Implemented bootstrap cards for document styles

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
       description: "asdfa asdfasd",
       file_url: 'http://google.com',
       updated_at: '3/21/18',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     },
     {
       title: "My Second Doc",
       description: "asdfa asdfasd",
       file_url: 'http://google.com',
       updated_at: '3/21/18',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/7/71/Freelancer_Illustration.png'
     },
     {
       title: "My Last Doc",
       description: "asdfa asdfasd",
       file_url: 'http://google.com',
       updated_at: '3/21/18',
-      image_url: 'http://google.com'
+      image_url: 'https://www.neoncrm.com/wp-content/uploads/2017/09/accounting-graphic.png'
     }
   ]
 }
