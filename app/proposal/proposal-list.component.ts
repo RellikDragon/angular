dbd98b1e3a1a1c0e2a763d08dd135b7ae23bc17c
Jared Dunn <rellikdragon@yahoo.com>
2018-03-29 12:39:42 -0600

    Implemented structure for api calls for data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService} from './proposal.service';
 
 @Component ({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list.component.scss']
+  styleUrls: ['proposal-list.component.scss'],
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'https://portfolio.jareddunn.com', 'Ruby on Rails', 150, 120, 15, 'jared@dunn.com')
-  proposalTwo: Proposal = new Proposal(99, 'Stuff Company', 'https://portfolio.jareddunn.com', 'Ruby on Rails', 150, 120, 15, 'jared@dunn.com')
-  proposalThree: Proposal = new Proposal(300, 'Sure Company', 'https://portfolio.jareddunn.com', 'Ruby on Rails', 150, 120, 15, 'jared@dunn.com')
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
 
-  proposals: Proposal[] = [
-    this.proposalOne,
-    this.proposalTwo,
-    this.proposalThree
-  ]
+  constructor(
+    private proposalService: ProposalService
+    ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }  
 }
